6b23f4462aed2545db3d83a09d7a6ae19ccd1d3c
opcc <opcc22059@gmail.com>
2024-07-30 14:55:34 +0800

    upgrade zksync depends

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-node";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
 
-import {apiKey} from "./.secret"
-
-module.exports = {
-  zksolc: {
-    version: "1.3.8",
-    compilerSource: "binary",
-    settings: {},
-  },
-  //defaultNetwork: "zkSyncTestnet",
-  defaultNetwork: "zkSyncMainnet",
+const config: HardhatUserConfig = {
+  defaultNetwork: "zklinkNova",
   networks: {
+    zklinkNovaTestnet: {
+      url: "https://goerli.rpc.zklink.io",
+      ethNetwork: "goerli",
+      zksync: true,
+      // verifyURL: "https://goerli.explorer.zklink.io/contract_verification",
+    },
+    zklinkNova: {
+      url: 'https://rpc.zklink.io',
+      ethNetwork: 'mainnet',
+      zksync: true,
+      verifyURL: 'https://explorer.zklink.io/contract_verification',
+    },
     zkSyncTestnet: {
       url: "https://testnet.era.zksync.dev",
       ethNetwork: "goerli", // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
@@ -23,12 +30,24 @@ module.exports = {
       ethNetwork: "https://mainnet.infura.io/v3/",
       zksync: true,
       verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-    }
-  },
-  solidity: {
-    version: "0.8.18",
+    },
+    hardhat: {
+      zksync: true,
+    },
   },
   etherscan: {
-    apiKey: apiKey,
-  }
+    apiKey: ""
+  },
+  zksolc: {
+    version: "1.3.22",
+    settings: {
+      // find all available options in the official documentation
+      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
+    },
+  },
+  solidity: {
+    version: "0.8.16",
+  },
 };
+
+export default config;
